9e86664e7e4dc5a19570e03a546aec4ac1adada7
phuocpro1969 <phuocpro1969@gmail.com>
2021-10-13 15:00:58 +0700

    update code

--- src/actions/account.js
+++ src/actions/account.js
@@ -1,72 +1,72 @@
-// import {
-// 	AccountLayout,
-// 	MintLayout,
-// 	Token,
-// 	TOKEN_PROGRAM_ID,
-// } from "@solana/spl-token";
-// import { WRAPPED_SOL_MINT } from "../utils/ids";
+import {
+	AccountLayout,
+	MintLayout,
+	Token,
+	TOKEN_PROGRAM_ID,
+} from "@solana/spl-token";
+import { WRAPPED_SOL_MINT } from "../utils/ids";
 
-// import {
-// 	Account,
-// 	PublicKey,
-// 	SystemProgram,
-// 	TransactionInstruction,
-// } from "@solana/web3.js";
+import {
+	Account,
+	PublicKey,
+	SystemProgram,
+	TransactionInstruction,
+} from "@solana/web3.js";
 
-// export function ensureSplAccount(
-// 	instructions,
-// 	cleanupInstructions,
-// 	toCheck,
-// 	payer,
-// 	amount,
-// 	signers
-// ) {
-// 	if (!toCheck.info.isNative) {
-// 		return toCheck.pubKey;
-// 	}
+export function ensureSplAccount(
+	instructions,
+	cleanupInstructions,
+	toCheck,
+	payer,
+	amount,
+	signers
+) {
+	if (!toCheck.info.isNative) {
+		return toCheck.pubKey;
+	}
 
-// 	const account = createUninitializedAccount(
-// 		instructions,
-// 		payer,
-// 		amount,
-// 		signers
-// 	);
+	const account = createUninitializedAccount(
+		instructions,
+		payer,
+		amount,
+		signers
+	);
 
-// 	instructions.push(
-// 		Token.createInitAccountInstruction(
-// 			TOKEN_PROGRAM_ID,
-// 			WRAPPED_SOL_MINT,
-// 			account,
-// 			payer
-// 		)
-// 	);
+	instructions.push(
+		Token.createInitAccountInstruction(
+			TOKEN_PROGRAM_ID,
+			WRAPPED_SOL_MINT,
+			account,
+			payer
+		)
+	);
 
-// 	cleanupInstructions.push(
-// 		Token.createCloseAccountInstruction(
-// 			TOKEN_PROGRAM_ID,
-// 			account,
-// 			payer,
-// 			payer,
-// 			[]
-// 		)
-// 	);
+	cleanupInstructions.push(
+		Token.createCloseAccountInstruction(
+			TOKEN_PROGRAM_ID,
+			account,
+			payer,
+			payer,
+			[]
+		)
+	);
 
-// 	return account;
-// }
+	return account;
+}
 
-// function createUninitializedAccount(instructions, payer, amount, signers) {
-// 	const account = new Account();
-// 	instructions.push(
-// 		SystemProgram.createAccount({
-// 			fromPubkey: payer,
-// 			newAccount: account.publicKey,
-// 			lamports: amount,
-// 			space: AccountLayout.span,
-// 			programId: TOKEN_PROGRAM_ID,
-// 		})
-// 	);
+function createUninitializedAccount(instructions, payer, amount, signers) {
+	const account = new Account();
+	instructions.push(
+		SystemProgram.createAccount({
+			fromPubkey: payer,
+			newAccount: account.publicKey,
+			lamports: amount,
+			space: AccountLayout.span,
+			programId: TOKEN_PROGRAM_ID,
+		})
+	);
 
-// 	signers.push(account);
+	signers.push(account);
 
-// 	return account.publicKey;
-// }
+	return account.publicKey;
+}
