12c852f3c1656e769c8d04f8e1fd40e586c6fb7f
phuocpro1969 <phuocpro1969@gmail.com>
2021-10-13 14:48:54 +0700

    update code

--- src/App.js
+++ src/App.js
@@ -1,25 +1,58 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+
+import {
+	getLedgerWallet,
+	getMathWallet,
+	getPhantomWallet,
+	getSolflareWallet,
+	getSolletWallet,
+	getSolongWallet,
+	getTorusWallet,
+} from "@solana/wallet-adapter-wallets";
+import { useMemo } from "react";
+import { Home } from "./pages/home";
+import { HashRouter, Route, Switch } from "react-router-dom";
+import { WalletProvider } from "@solana/wallet-adapter-react";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	const wallets = useMemo(
+		() => [
+			getPhantomWallet(),
+			getSolflareWallet(),
+			getTorusWallet({
+				options: {
+					// TODO: Get your own tor.us wallet client Id
+					clientId:
+						"BOM5Cl7PXgE9Ylq1Z1tqzhpydY0RVr8k90QQ85N7AKI5QGSrr9iDC-3rvmy0K_hF0JfpLMiXoDhta68JwcxS1LQ",
+				},
+			}),
+			getLedgerWallet(),
+			getSolongWallet(),
+			getMathWallet(),
+			getSolletWallet(),
+		],
+		[]
+	);
+	return (
+		<HashRouter basename={"/"}>
+			{/* <ConnectionProvider> */}
+			<WalletProvider wallets={wallets} autoConnect>
+				{/* <AccountsProvider>
+						<MarketProvider> */}
+				<Switch>
+					<Route exact path="/" component={() => <Home />} />
+					{/* <Route
+										exact
+										path="/faucet"
+										children={<FaucetView />}
+									/> */}
+				</Switch>
+				{/* </MarketProvider>
+					</AccountsProvider> */}
+			</WalletProvider>
+			{/* </ConnectionProvider> */}
+		</HashRouter>
+	);
 }
 
 export default App;
